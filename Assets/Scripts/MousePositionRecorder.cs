bc91acc7a9dbc8101fe02f995b006e4041f85bfb
Marlon Gilliam <14471455@students.lincoln.ac.uk>
2017-09-28 15:43:43 +0100

    Gesture recognition system Implimented
    Works in 2d space with mouse input.

--- Assets/Scripts/MousePositionRecorder.cs
+++ Assets/Scripts/MousePositionRecorder.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 using Accord.Statistics.Models.Markov;
 using Accord.Statistics.Models.Markov.Topology;
@@ -15,8 +16,16 @@ using System.Linq;
 
 public class MousePositionRecorder : MonoBehaviour
 {
+    public Button StoreGestureBtn;
+    public Button LearnGesturesBtn;
+    public Button PredictGestureBtn;
+
+    public InputField nameInputField;
+    public Text text;
+
     List<Vector3> mousePositions;
-    List<double[][]> storedGestures;
+    List<Gesture> storedGestures;
+    Dictionary<string, int> gestureIndex;
     bool _isRecording;
 
     HiddenMarkovClassifier<MultivariateNormalDistribution, double[]> hmm;
@@ -25,7 +34,12 @@ public class MousePositionRecorder : MonoBehaviour
 	void Start ()
     {
         mousePositions = new List<Vector3>();
-        storedGestures = new List<double[][]>();
+        storedGestures = new List<Gesture>();
+        gestureIndex = new Dictionary<string, int>();
+
+        StoreGestureBtn.onClick.AddListener(() => StoreGesture(mousePositions, nameInputField.text));
+        LearnGesturesBtn.onClick.AddListener(() => LearnGesture());
+        PredictGestureBtn.onClick.AddListener(() => CheckRecognized(mousePositions));
 	}
 	
     void HandleInput()
@@ -68,19 +82,23 @@ public class MousePositionRecorder : MonoBehaviour
     void EndRecording()
     {
         Debug.Log("Recording Ended!");
-        StoreGesture(mousePositions);
         _isRecording = false;
     }
 
-    void StoreGesture(List<Vector3> positions)
+    void StoreGesture(List<Vector3> positions, string name)
     {
         double[][] points = new double[positions.Count][];
         for (int i = 0; i < positions.Count; i++)
         {
             points[i] = new double[3] { positions[i].x, positions[i].y, positions[i].z };
         }
-        storedGestures.Add(points);
-        Debug.Log("Gesture Recorded!");
+        if (!gestureIndex.ContainsKey(name))
+        {
+            gestureIndex.Add(name, gestureIndex.Count);
+        }
+        Gesture gesture = new Gesture(points, name, gestureIndex[name]);
+        storedGestures.Add(gesture);
+        Debug.Log("Gesture Recorded as: " + name);
     }
 
     void LearnGesture()
@@ -90,8 +108,8 @@ public class MousePositionRecorder : MonoBehaviour
 
         for (int i = 0; i < inputs.Length; i++)
         {
-            inputs[i] = storedGestures[i];
-            outputs[i] = 0;
+            inputs[i] = storedGestures[i].points;
+            outputs[i] = storedGestures[i].index;
         }
 
         int states = 5;
@@ -121,8 +139,6 @@ public class MousePositionRecorder : MonoBehaviour
 
         teacher.Learn(inputs, outputs);
 
-        storedGestures.Clear();
-
         Debug.Log("Sequence Learned!");
     }
 
@@ -137,6 +153,27 @@ public class MousePositionRecorder : MonoBehaviour
         }
 
         Debug.Log(hmm.Models[0].LogLikelihood(points));
-        Debug.Log(hmm.Decide(points));
+        int decision = hmm.Decide(points);
+        string value = string.Empty;
+        foreach (KeyValuePair<string, int> item in gestureIndex)
+        {
+            if (item.Value == decision)
+            { value = item.Key; }
+        }
+        Debug.Log("Did you write a: " + value + "?");
+    }
+}
+
+public struct Gesture
+{
+    public double[][] points;
+    public string name;
+    public int index;
+
+    public Gesture(double[][] points, string name, int index)
+    {
+        this.points = points;
+        this.name = name;
+        this.index = index;
     }
 }
